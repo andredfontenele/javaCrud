ab9860166431075509ed163bef3b62e6d7ff8d64
Andre <andrefonteneledev@gmail.com>
2023-12-06 11:31:34 -0300

    Projeto reiniciado e conector mysql atualizado

--- src/br/com/agenda/factory/ConnectionFactory.java
+++ src/br/com/agenda/factory/ConnectionFactory.java
@@ -9,8 +9,9 @@ public class ConnectionFactory {
 	
 	private static final String PASSWORD="root";
 	
-	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/agenda";
+	private static final String DATABASE_URL="jdbc:mysql://localhost:3306/agenda";
 	
+	//Conexão com o banco de dados
 	public static Connection createConnectionToMySQL() throws Exception{
 		Class.forName("com.mysql.jdbc.Driver");
 		
@@ -19,18 +20,11 @@ public class ConnectionFactory {
 		return connection;
 	}
 	
-	public static void main(String[] args) throws Exception{
-		//Recuperar conexão
+	public static void main(String[] args) {
+		//Recuperar conexão com o bd
 		Connection conn = createConnectionToMySQL();
 		
-		//Testar se a conexão é nula
-		if(conn!=null) {
-			System.out.println("Conexão obtida com sucesso.");
-			conn.close();
-		}
+		if
 	}
-	
-	
-
 	
 }
